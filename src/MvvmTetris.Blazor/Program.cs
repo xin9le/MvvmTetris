ef522a70e17065acfaddfbfca36dca4fc246c9c5
Takaaki Suzuki <xin9le@live.jp>
2019-05-08 01:09:56 +0900

    Add Blazor project

---
+++ src/MvvmTetris.Blazor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+
+
+namespace MvvmTetris.Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => BlazorWebAssemblyHost.CreateDefaultBuilder()
+            .UseBlazorStartup<Startup>()
+            .Build()
+            .Run();
+    }
+}
